0169b2e83ec0be553899f3ca0e2f02a44afe2844
hua24 <hua2424242424@gmail.com>
2022-03-18 15:40:56 +0800

    1

--- app/src/main/java/com/example/myapplication3/Activity_manager.java
+++ app/src/main/java/com/example/myapplication3/Activity_manager.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Activity_manager {
-    //顶顶顶sssss
+    //顶顶顶ss
     public static  List<Activity> activities = new ArrayList<Activity>();
     public static void addActivity(Activity activity) {
         activities.add(activity);
